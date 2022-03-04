8a96c2b7c61a0eb46b9f15a58ef107f52408cb10
MathiasReker <{ID}+{username}@users.noreply.github.com>
2022-03-04 22:41:43 +0100

    init

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
-const url = "http://worldtimeapi.org/api/timezone/";
+const url = "https://worldtimeapi.org/api/timezone/",
+  timezoneDropdown = document.getElementById("timezone-dropdown"),
+  time = document.getElementById("time"),
+  city = document.getElementById("timezone-city"),
+  continent = document.getElementById("timezone-continent"),
+  UTC = document.getElementById("UTC");
 
-const dropdown = document.getElementById("dropdown");
-dropdown.addEventListener("change", () => {
-  console.log(dropdown.value);
-  worldtimeapiFetch(url + dropdown.value);
-});
+function fillDropdown() {
+  fetch(url).then(t => t.json()).then(t => t.forEach(t => {
+    if (t.includes("/") && !t.includes("Etc")) {
+      const e = document.createElement("option"),
+        n = t.split("/");
+      e.textContent = n[0] + " (" + n[1].replace("_", " ") + ")";
+      e.value = t;
+      timezoneDropdown.appendChild(e);
+    }
+  })).catch(t => console.error(t));
+}
 
-const worldtimeapiFetch = function (url) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      /*{
-  "abbreviation": "CET",
-  "client_ip": "87.59.14.119",
-  "datetime": "2022-02-17T21:01:50.952225+01:00", <----
-  "day_of_week": 4,
-  "day_of_year": 48,
-  "dst": false,
-  "dst_from": null,
-  "dst_offset": 0,
-  "dst_until": null,
-  "raw_offset": 3600,
-  "timezone": "Europe/Copenhagen", <----
-  "unixtime": 1645128110,
-  "utc_datetime": "2022-02-17T20:01:50.952225+00:00",
-  "utc_offset": "+01:00", <----
-  "week_number": 7
-        }*/
+function worldTimeApiFetch() {
+  fetch(url + timezoneDropdown.value).then(t => t.json()).then(t => {
+    const e = t.timezone.split("/");
+    time.textContent = t.datetime.substring(11, 19);
+    continent.textContent = e[0];
+    city.textContent = e[1].replace("_", " ");
+    UTC.textContent = t.utc_offset;
+  }).catch(t => console.error(t));
+  setTimeout(worldTimeApiFetch, 1e3);
+}
 
-      /*TODO: lav passende attributter ud fra de markerede JSON værdier som kan manipulerer med klokken  */
-      /* Altså vælg tidszone fra drop down, og set uret.
-    });
-};
+fillDropdown();
+timezoneDropdown.addEventListener("change", worldTimeApiFetch);
